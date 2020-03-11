f73b350742e6205b102bf39eb88bf1b21b29d755
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2020-03-11 16:03:54 +0100

    Refactor data splitting for training the baseline

--- src/data/split_europarl.py
+++ src/data/split_europarl.py
@@ -1,16 +1,17 @@
+import os
 import logging
 from tqdm import tqdm
 import click
 import numpy as np
 
 
-LANGUAGES = {
+languages = {
     'bg',
     'cs',
     'da',
     'de',
     'en',
-    # 'el',  # We nous have encoding problems with el
+    # 'el',  # we nous have encoding problems with el
     'es',
     'et',
     'fi',
@@ -30,7 +31,7 @@ LANGUAGES = {
 
 
 def write_split(filename, split, lines):
-    with open("{}.{}".format(filename, split), 'w') as fhandle:
+    with open("{}/{}".format(filename, split), 'w') as fhandle:
         for line in lines:
             fhandle.write(line)
 
@@ -49,19 +50,31 @@ def split_dataset(filename, train_ratio, valid_ratio):
     valid_lines = lines[train_split_index:valid_split_index]
     test_lines = lines[valid_split_index:]
 
-    write_split(filename, 'train', train_lines)
-    write_split(filename, 'valid', valid_lines)
-    write_split(filename, 'test', test_lines)
+    os.makedirs(filename + ".split/train", exist_ok=True)
+    os.makedirs(filename + ".split/valid", exist_ok=True)
+    os.makedirs(filename + ".split/test", exist_ok=True)
+    write_split(filename + ".split/train", 'data.txt', train_lines)
+    write_split(filename + ".split/valid", 'data.txt', valid_lines)
+    write_split(filename + ".split/test", 'data.txt', test_lines)
 
 
 @click.command()
 @click.option('--train-ratio', default=0.6)
 @click.option('--valid-ratio', default=0.2)
 def main(train_ratio, valid_ratio):
+
+    # Special case for english
+    logging.info("Tokenizing en corpus")
+    split_dataset(
+        './data/raw/europarl/es-en/europarl-v7.es-en.en',
+        train_ratio,
+        valid_ratio
+    )
+
     for lang in LANGUAGES:
         logging.info("Splitting {} corpus".format(lang))
         split_dataset(
-            './data/processed/europarl.tokenized.{}'.format(lang),
+            './data/raw/europarl/{}-en/europarl-v7.{}-en.{}'.format(lang, lang, lang),
             train_ratio,
             valid_ratio
         )
--- src/models/train_baseline.py
+++ src/models/train_baseline.py
@@ -1,10 +1,39 @@
 from fastai.text import *
+from fastai.text.data import *
+
+
+LANGUAGES = {
+    'bg',
+    'cs',
+    'da',
+    'de',
+    'en',
+    'es',
+    'et',
+    'fi',
+    'fr',
+    'hu',
+    'it',
+    'lt',
+    'lv',
+    'nl',
+    'pl',
+    'pt',
+    'ro',
+    'sk',
+    'sl',
+    'sv'
+}
+
+
+def get_databunch(path):
+    databunch = TextLMDataBunch.from_folder(path)
+    return databunch
+
 
 if __name__ == '__main__':
-    sentences = list()
-    with open("./data/processed/europarl.tokenized.sl.train") as fhandle:
-        for line in fhandle:
-            sentences.append(line[:-1])
-    d = TextList(sentences).split_none().label_for_lm().databunch()
-    learn = language_model_learner(d, AWD_LSTM, pretrained=False)
-    learn.fit_one_cycle(10, 1e-2)
+    for lang in LANGUAGES:
+        split_path = './data/raw/europarl/{}-en/europarl-v7.{}-en.{}.split'.format(lang, lang, lang)
+        d = get_databunch(split_path)
+        learn = language_model_learner(d, AWD_LSTM, pretrained=False)
+        learn.fit_one_cycle(10, 1e-2)
