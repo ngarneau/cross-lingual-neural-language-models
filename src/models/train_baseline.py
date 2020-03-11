20d3be4ea5ef0d30a1e1fc3eff7a8d11f9dc24f2
Nicolas Garneau <nicolas.garneau.1@ulaval.ca>
2020-03-11 16:12:34 +0100

    Adding perplexity and training as in the fastai example

--- src/models/train_baseline.py
+++ src/models/train_baseline.py
@@ -1,6 +1,8 @@
 from fastai.text import *
 from fastai.text.data import *
 
+import logging
+
 
 LANGUAGES = {
     'bg',
@@ -32,8 +34,10 @@ def get_databunch(path):
 
 
 if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
     for lang in LANGUAGES:
+        logging.info("Starting training for {}".format(lang))
         split_path = './data/raw/europarl/{}-en/europarl-v7.{}-en.{}.split'.format(lang, lang, lang)
         d = get_databunch(split_path)
-        learn = language_model_learner(d, AWD_LSTM, pretrained=False)
-        learn.fit_one_cycle(10, 1e-2)
+        learn = language_model_learner(d, AWD_LSTM, pretrained=False, metrics=[Perplexity()])
+        learn.fit_one_cycle(90, 5e-3, moms=(0.8,0.7,0.8), div=10)
